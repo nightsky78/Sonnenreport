168b69a737f1a61329b0783a43ea8bd6a66ed067
johannes <johannes.hettig@protonmail.com>
2023-04-01 21:43:04 +0200

    first version finished to start logging

--- data_store.py
+++ data_store.py
@@ -1,12 +1,43 @@
 import data_request
 import json
 import config
+import db_handler
+import time
+import logging
+
+# first set the log level
+logging.basicConfig(level=logging.DEBUG)
 
 # get the URL from the config file
 url = config.get_url()
 
 # get the data
-# loads function converts JSON-formatted string into a Python dictionary object. The name "loads" stands for "load string", indicating that the function operates on a string rather than a file.
-data = json.loads(data_request.get_data(url))
+# loads function converts JSON-formatted string into a Python dictionary object. The 
+# name "loads" stands for "load string", indicating that the function operates on a string rather than a file.
 
-print(data["Apparent_output"])
+while True:
+        try:
+            data = json.loads(data_request.get_data(url))
+        except:
+              logging.critical('Data request failed')
+
+        logging.debug(f" The return data is: {data['Apparent_output']}, {data['BatteryCharging']}, {data['BatteryDischarging']}, {data['GridFeedIn_W']}, {data['Consumption_Avg']}, {data['Consumption_W']}, {data['Production_W']}, {data['USOC']}, {data['Timestamp']}, {data['RemainingCapacity_Wh']}")
+
+
+        # now we need to store the data in the DB
+        try:
+              db = db_handler.Database('sonnen_data.db')
+        except:
+              logging.critical(' Data request failed.')
+
+        
+        try:
+              db.insert_dataset(data["Apparent_output"], data["BatteryCharging"], data["BatteryDischarging"], 
+                        data["GridFeedIn_W"], data["Consumption_Avg"], data["Consumption_W"], 
+                        data["Production_W"], data["USOC"], data["Timestamp"], data["RemainingCapacity_Wh"] )
+        except:
+              logging.critical(" Data Insert failed.")
+        
+        time.sleep(10)
+
+
--- db_handler.py
+++ db_handler.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-class PersonDatabase:
+class Database:
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
         
     
-    def insert_dataset(self, output_num, charging, discharging, consumption_avg, consumption_w, production_w, rsoc, timestamp, remaining_capacity_wh):
-        self.cursor.execute("INSERT INTO example_table (output_num, charging, discharging, consumption_avg, consumption_w, production_w, rsoc, timestamp, remaining_capacity_wh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (output_num, charging, discharging, consumption_avg, consumption_w, production_w, rsoc, timestamp, remaining_capacity_wh))
+    def insert_dataset(self, output_num, charging, discharging, GridFeedIn_W,  consumption_avg, consumption_w, production_w, usoc, timestamp, remaining_capacity_wh):
+        self.cursor.execute("INSERT INTO sonnendata (output_num, charging, discharging, GridFeedIn_W, consumption_avg, consumption_w, production_w, usoc, timestamp, remaining_capacity_wh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (output_num, charging, discharging, GridFeedIn_W, consumption_avg, consumption_w, production_w, usoc, timestamp, remaining_capacity_wh))
         self.conn.commit()
     
     def select_all(self):
-        self.cursor.execute("SELECT * FROM people")
+        self.cursor.execute("SELECT * FROM sonnendata")
         return self.cursor.fetchall()
     
     def get_records_by_date_range(self, start_date, end_date):
-        query = "SELECT name, age FROM people WHERE date_created BETWEEN ? AND ?"
+        query = "SELECT * FROM sonnendata WHERE date_created BETWEEN ? AND ?"
         self.cursor.execute(query, (start_date, end_date))
         records = self.cursor.fetchall()
         return records
